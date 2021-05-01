10f5c5f3465d6e6df55e9c37d467ea6feea2e137
Rune Finstad Halvorsen <runefh@gmail.com>
2021-05-02 01:53:58 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,28 @@
 import { resolve } from 'path';
 import { OpenAPIV3 } from 'openapi-types';
 import { dereference } from 'swagger-parser';
-import { schemaToType } from './type-parser';
 import invariant from 'ts-invariant';
-
-type PathItemObject = OpenAPIV3.PathItemObject;
-type OperationObject = OpenAPIV3.OperationObject;
+import { schemaToType } from './type-parser';
 
 const definitionPath = resolve(__dirname, '../resources/fiken.yaml');
 
-// function parseOperation(op: OperationObject) {
-//   const {
-//     deprecated,
-//     description,
-//     operationId,
-//     parameters,
-//     summary,
-//     requestBody,
-//   } = op;
-// }
-
-function parsePath(item: PathItemObject) {
-  if (item.get) {
-    // parseOperation(item.get);
-  }
-}
-
-interface Param {
-  name: string;
-  location: 'query' | 'path';
-  deprecated?: boolean;
-}
+type HttpMethod =
+  | 'get'
+  | 'put'
+  | 'post'
+  | 'delete'
+  | 'options'
+  | 'head'
+  | 'patch'
+  | 'trace';
 
 // function parseParameters(params: OpenAPIV3.ParameterObject) {
 
 // }
+
+function isDefined<T>(value: T | undefined | null): value is T {
+  return typeof value !== 'undefined' && value !== null;
+}
 
 function isParam(
   thing: OpenAPIV3.ReferenceObject | OpenAPIV3.ParameterObject,
@@ -61,20 +49,55 @@ function getParamKind(str: string): ParamKind {
   throw new Error(`Invalid param kind "${str}"`);
 }
 
+function parseMethod(
+  method: OpenAPIV3.HttpMethods,
+  operation: OpenAPIV3.OperationObject,
+): { method: OpenAPIV3.HttpMethods; operation: any } {
+  return { method, operation };
+}
+
+function parsePath(path: string, item: OpenAPIV3.PathItemObject) {
+  const paramsForPath = item.parameters?.filter(isParam).map((e) => {
+    invariant(isSchemaObject(e.schema));
+    return {
+      path,
+      name: e.name,
+      kind: getParamKind(e.in),
+      types: schemaToType(e.schema),
+      required: e.required ?? false,
+    };
+  });
+
+  const operations = Object.values(OpenAPIV3.HttpMethods)
+    .map((e) => {
+      const method = item[e];
+      return method ? parseMethod(e, method) : undefined;
+    })
+    .filter(isDefined);
+
+  return operations;
+}
+
 async function main() {
   const v4 = await dereference(definitionPath);
 
-  for (const [_path, val] of Object.entries(v4.paths)) {
-    const item: PathItemObject = val;
+  const pathMethods = Object.entries(v4.paths).map(([path, item]) =>
+    parsePath(path, item),
+  );
+
+  for (const [path, val] of Object.entries(v4.paths)) {
+    const item: OpenAPIV3.PathItemObject = val;
     if (item.parameters) {
       invariant(item.parameters.every(isParam), 'not every');
       const params = item.parameters.map((e) => {
         invariant(isSchemaObject(e.schema), 'not schema');
         // invariant
         return {
+          path,
           name: e.name,
           kind: getParamKind(e.in),
           types: schemaToType(e.schema),
+          required: e.required ?? false,
         };
       });
 
